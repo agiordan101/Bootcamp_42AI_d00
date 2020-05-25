b5cc0042f87e4dcbd4c2b3a465377696c0e9d68a
agiordan101 <agd611@gmail.com>
2020-05-25 20:19:56 +0200

    save

--- ex04/operations.py
+++ ex04/operations.py
@@ -1,4 +1,20 @@
 import sys
 
-if len(sys.argv) > 3 or len(sys.argv) < 3:
-    print()
+if len(sys.argv) != 3 or
+    sys.argv[1].isnumeric is not True or
+    sys.argv[2].isnumeric is not True:
+    if (len(sys.argv) < 3):
+    elif (len(sys.argv) > 3):
+        print("InputError: too many arguments\n")
+    else:
+        print("InputError: only numbers\n")
+    print("Usage: python operations.py <number1> <number2>")
+    print("Example:")
+    print("    python operations.py 10 3")
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+print("Sum:" + str(a + b))
+print("Difference:\t" + str(a - b))
+print("Product:\t\t" + str(a * b))
+print("Quotient:\t\t" + str(a / b))
+print("Reminder:\t\t" + str(a % b))
